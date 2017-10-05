a73ba147b2dba0bd4f33bc2a5faf835e3676de15
DWW1993 <daniel.w.walker1993@gmail.com>
2017-10-05 18:05:15 -0500

    just have to randomize accusations

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 var friends = ["Jordan", "Corey", "Reagan", "Nick", "Will"],
     locations = ["the classroom", "Makarios", "Taco Bell", "New China Town", "The Dan", "the break room", "the depot", "Lordran", "Castle Black", "The Shire"],
     weapons = ["big gun", "Moonlight Greatsword", "golden gun", "gigantic knife", "hockey puck", "machine gun", "claymore", "needler", "fist", "baseball bat wrapped in barbed wire", "golf club", "iMac", "chess board", "AK47", "piano wire", "exploding Galaxy Note 7", "the keyboard lab", "dangerous weapon", "tactical nuke", "this lab"];
-
+    var randFriend,
+        randLocation,
+        randWeapon;
+    
 $(document).ready(function(){
     for (a = 1; a < 101; a++) {
-        $(document.body).append(`<h3 id="${a}">~ ~ ~ Accusation ${a} ~ ~ ~</h3>`)
-        var alrt = `I accuse ${rand(friends)}, with the ${rand(weapons)} in ${rand(locations)}`;
-        
+        randFriend = rand(friends),
+        randLocation = rand(locations),
+        randWeapon = rand(weapons);
+        var addh3 = $(`<h3 id="${a}">~ ~ ~ Accusation ${a} ~ ~ ~</h3>`);
+        $(document.body).append(addh3);
+
+        $(addh3).click(function(){
+            al();
+        });
     }
-    $("h3").click(function(){
-        alert(`Accusation ${this.id}: ${alrt}`);
-    });
 });
 
 function rand(array) {
-    return array[Math.floor(Math.random() * array.length)];
+    return array[Math.floor(Math.random() * array.length) + 0];
 }
+
+var al = function() {
+    alert(`Accusation: I accuse ${randFriend}, with the ${randWeapon} in ${randLocation}`);
+    }
